30841e49c5441e9e31ac2a2383f32ab1ab2d870c
tnnz20 <kaitora200@gmail.com>
2023-07-25 21:25:34 +0800

    feat: add method for Playlist

--- models/Playlist.js
+++ models/Playlist.js
@@ -2,6 +2,21 @@ class Playlists {
     constructor(){
         this.songs = []
     }
+    addSong(song){
+        this.songs.push(song)
+    }
+
+    getSongs(){
+        return this.songs
+    }
+
+    playSong(index){
+        return this.songs[index]
+    }
+
+    getMostPlayed(){
+        return this.songs.sort((a,b) => b.totalPlay - a.totalPlay)
+    }
 }
 
 export default Playlists
